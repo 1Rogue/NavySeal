c555892e3baf6a250513460a03f71611e7cf2d8b
Spencer Alderman <spencer@1rogue.com>
2015-02-19 20:11:51 -0500

    xml is for squares

--- src/main/java/com/codelanx/navyseal/MessengerThread.java
+++ src/main/java/com/codelanx/navyseal/MessengerThread.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Spencer
+ * Copyright (C) 2015 No one
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/java/com/codelanx/navyseal/NavySeal.java
+++ src/main/java/com/codelanx/navyseal/NavySeal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Codelanx
+ * Copyright (C) 2015 Your mom
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
